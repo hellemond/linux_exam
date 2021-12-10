ee61d5f9a765b26f79a92e60571512edf26cf17b
Hellemond <hdargalaii@gmail.com>
2021-12-10 17:41:40 +0000

    first commit

---
+++ bckup.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+FILE="${HOME}/exam"
+
+
+while getopts ":d" arg; do
+  case $arg in
+    d)
+      	    
+      echo "tar -cvzf $FILE/${OPTARG}.tar.gz"
+      echo "rsync --remove-source-files -options $FILE/$OPTARG.tar.gz $FILE/backups" 
+      
+      ;;
+    *)
+      echo "$arg is invalid"
+      ;;
+  esac
+done
+
+
