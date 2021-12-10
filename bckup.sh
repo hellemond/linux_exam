d934f711c18a262aba05e348a451556f1e734fa4
Hellemond <hdargalaii@gmail.com>
2021-12-10 17:57:06 +0000

    backup.sh

--- bckup.sh
+++ bckup.sh
@@ -5,15 +5,13 @@ FILE="${HOME}/exam"
 while getopts ":d" arg; do
   case $arg in
     d)
-      	    
+
       echo "tar -cvzf $FILE/${OPTARG}.tar.gz"
-      echo "rsync --remove-source-files -options $FILE/$OPTARG.tar.gz $FILE/backups" 
-      
+      echo "rsync --remove-source-files -options $FILE/$OPTARG.tar.gz $FILE/backups"
+
       ;;
     *)
       echo "$arg is invalid"
       ;;
   esac
 done
-
-
